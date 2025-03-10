4591fb1d35cbe8a2ac594bcc7260f391b56090fe
osama11 <osamamadi43@gmail.com>
2025-03-10 15:01:54 +0200

    AAA.PYTHON

--- aaa.py
+++ aaa.py
@@ -1,3 +1,7 @@
-a = 1+(7) 
-print(a) 
+def func1():
+    return "Hiiiiiiiiiii :)"
+
+a = 1 + (7)
+print(a)
 print("Fake addition to allow u pull the code :)")
+print("Diesel Stop " + func1())  # Now it works!
