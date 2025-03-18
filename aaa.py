428c83d8948897eedf839938651cd2d91dfe9a6b
osamamadi <osamamadi43@gmail.com>
2025-03-18 17:07:48 +0200

    Tutorials using freameworks GitLens and more

--- aaa.py
+++ aaa.py
@@ -2,3 +2,7 @@ a = 1 + 7
 print(a)
 print("Fake addition to allow u pull the code :)")
 print("Diesel Stop " + "Hiiiiiiiiiii :)")  # Now it works Effiiently :(
+print("OEKEOEEOEOOE")
+i = 0
+for i in range(10):
+    print("Osama Madi")
